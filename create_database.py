041596be447b064d89be987dcebb3f3d961a61f2
AnyMac Store <anymacstore@MacBook-Air-AnyMac-3.local>
2023-08-26 00:59:06 +0300

    add fill db

--- create_database.py
+++ create_database.py
@@ -1,4 +1,7 @@
+import random
 import sqlite3
+
+import fake_data
 
 def connector(sql):
     with sqlite3.connect('my_data.db') as con:
@@ -11,7 +14,22 @@ def create_db():
     
     connector(sql)
 
+def fill_students():
+    randomise = random.randint(30,50)
+    sql_command = "\n".join(f"INSERT INTO students (student) VALUES ('{fake_data.student()}');" for _ in range(randomise))
+    connector(sql_command)
 
+def fill_teachers():
+    randomise = random.randint(3,5)
+    sql_command = "\n".join(f"INSERT INTO teachers (teacher) VALUES ('{fake_data.teacher()}');" for _ in range(randomise))
+    connector(sql_command)
+
+def fill_groups():
+    sql_command = "\n".join(f"INSERT INTO groups (group_name) VALUES ('{fake_data.group()}');" for _ in range(3))
+    connector(sql_command)
 
 if __name__ == "__main__":
     create_db()
+    fill_students()
+    fill_teachers()
+    fill_groups()
